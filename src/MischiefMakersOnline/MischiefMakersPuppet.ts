84db99efab67d4952ca6974e3352875d2d875958
Drahsid <Drahsid@gmail.com>
2020-07-08 16:45:59 -0400

    No longer allow the puppet to be placed at an index that is used for particle effects More closely mimick how the game constructs an afterimage for the construction of puppets

--- src/MischiefMakersOnline/MischiefMakersPuppet.ts
+++ src/MischiefMakersOnline/MischiefMakersPuppet.ts
@@ -65,16 +65,20 @@ export class PuppetOverlord {
         }
 
         for (i = 1; i < ACTOR_LIST_SIZE; i++) {
-            actor = new Actor(this.ModLoader.emulator, i);
-            if (!actor.is_alive) {
-                free_slot = i
-                this.ModLoader.logger.info("Found free slot at " + i.toString())
-                break
+            if (i > 0x2d) { // 0x10 - 0x2d are used for particles, let's just skip that whole chunk for now
+                actor = new Actor(this.ModLoader.emulator, i);
+                if (!actor.is_alive) {
+                    free_slot = i
+                    this.ModLoader.logger.info("Found free slot at " + i.toString())
+                    break
+                }
             }
         }
 
         if (free_slot > 0) {
             i = ACTOR_LIST_POINTER + (SIZEOF_ACTOR * free_slot)
+
+            let marina: Player = new Player(this.ModLoader.emulator, 0)
 
             this.puppets.push(new Puppet(this.ModLoader.emulator, free_slot, uuid))
 
@@ -82,10 +86,83 @@ export class PuppetOverlord {
             copy_data = this.ModLoader.emulator.rdramReadBuffer(ACTOR_LIST_POINTER, SIZEOF_ACTOR)
             this.ModLoader.emulator.rdramWriteBuffer(i, copy_data)
 
+            // For the most part, mimicking the game's behaviour when constructing an actor at X index (0x8001E250)
+            let type = 0x002E
+            let mode = this.ModLoader.emulator.rdramRead32(0x800C7DC8 + (type * 4))
+            let uptr = 0x800C7FBC + (type * 4)
+
             //0x0E, 0x0F, 0x21, 0x60, 0x61
+            this.puppets[this.puppets.length - 1].actor.type = type // afterimage uses 2E
+            this.puppets[this.puppets.length - 1].actor.mode = mode
+            this.ModLoader.emulator.rdramWrite32(i + 0xE8, uptr)
+
+            this.puppets[this.puppets.length - 1].actor.scale_0 = new Vector2(1, 1)
+            this.ModLoader.emulator.rdramWrite32(i + 0x14C, 0)
+            this.ModLoader.emulator.rdramWrite32(i + 0x148, 0)
+            this.ModLoader.emulator.rdramWrite32(i + 0x144, 0)
+            this.ModLoader.emulator.rdramWrite32(i + 0x140, 0)
+            this.ModLoader.emulator.rdramWrite32(i + 0x13C, 0)
+            this.ModLoader.emulator.rdramWrite32(i + 0x138, 0)
+            this.ModLoader.emulator.rdramWrite32(i + 0x134, 0)
+            this.ModLoader.emulator.rdramWrite32(i + 0x130, 0)
+            this.puppets[this.puppets.length - 1].actor.air_ground_state = 0
+            this.puppets[this.puppets.length - 1].actor.scale_1 = new Vector2(0, 0)
+            this.ModLoader.emulator.rdramWrite32(i + 0x11C, 0)
+            this.ModLoader.emulator.rdramWrite32(i + 0x118, 0)
+            this.ModLoader.emulator.rdramWrite32(i + 0x114, 0)
+            this.ModLoader.emulator.rdramWrite32(i + 0x110, 0)
+            this.ModLoader.emulator.rdramWrite8(i + 0x9C + 3, 0xFF) // RGBA + 3 (alpha)
+            this.ModLoader.emulator.rdramWrite16(i + 0xE6, 1)
+            this.ModLoader.emulator.rdramWrite8(i + 0xDE, 1)
+            this.ModLoader.emulator.rdramWrite32(i + 0xC4, 0)
+            this.ModLoader.emulator.rdramWrite32(i + 0xC0, 0)
+            this.ModLoader.emulator.rdramWrite32(i + 0xBC, 0)
+            this.ModLoader.emulator.rdramWrite16(i + 0xD6, 0)
+            this.ModLoader.emulator.rdramWrite16(i + 0xD4, 0)
             this.puppets[this.puppets.length - 1].actor.status = 0x0000
-            this.puppets[this.puppets.length - 1].actor.type = 0x0060
+            this.puppets[this.puppets.length - 1].actor.pos_4 =  new Vector2()
+            this.puppets[this.puppets.length - 1].actor.pos_3 = new Vector2()
+            this.ModLoader.emulator.rdramWrite32(i + 0x190, 0)
+            this.ModLoader.emulator.rdramWrite32(i + 0x18C, 0)
+            this.ModLoader.emulator.rdramWrite32(i + 0x188, 0)
+            this.ModLoader.emulator.rdramWrite32(i + 0x184, 0)
+            this.ModLoader.emulator.rdramWrite32(i + 0x180, 0)
+            this.ModLoader.emulator.rdramWrite32(i + 0x17C, 0)
+            this.ModLoader.emulator.rdramWrite32(i + 0x178, 0)
+            this.ModLoader.emulator.rdramWrite32(i + 0x174, 0)
+            this.puppets[this.puppets.length - 1].actor.anim_flags = 0
+            this.ModLoader.emulator.rdramWrite32(i + 0x16C, 0)
+            this.ModLoader.emulator.rdramWrite32(i + 0x168, 0)
+            this.ModLoader.emulator.rdramWrite32(i + 0x164, 0)
+            this.ModLoader.emulator.rdramWrite32(i + 0x160, 0)
+            this.ModLoader.emulator.rdramWrite32(i + 0x15C, 0)
+            this.ModLoader.emulator.rdramWrite32(i + 0x158, 0)
+            this.ModLoader.emulator.rdramWrite32(i + 0x154, 0)
+            this.puppets[this.puppets.length - 1].actor.idle_time = 0
+            this.ModLoader.emulator.rdramWrite32(i + 0x10C, 0)
+            this.ModLoader.emulator.rdramWrite32(i + 0x108, 0)
+            this.ModLoader.emulator.rdramWrite32(i + 0x104, 0)
+            this.ModLoader.emulator.rdramWrite32(i + 0x100, 0)
+            this.ModLoader.emulator.rdramWrite32(i + 0xFC, 0)
+            this.ModLoader.emulator.rdramWrite32(i + 0xF8, 0)
+            this.puppets[this.puppets.length - 1].actor.velocity = new Vector2()
+            this.ModLoader.emulator.rdramWrite16(i + 0xE2, 0)
+            this.ModLoader.emulator.rdramWrite8(i + 0xDF, 0)
+            this.ModLoader.emulator.rdramWrite8(i + 0xDD, 0)
+            this.ModLoader.emulator.rdramWrite8(i + 0xDC, 0)
+            this.ModLoader.emulator.rdramWrite8(i + 0xDB, 0)
+            this.ModLoader.emulator.rdramWrite8(i + 0xDA, 0)
+            this.ModLoader.emulator.rdramWrite32(i + 0x98, 0)
+            this.puppets[this.puppets.length - 1].actor.effect_flags = 0
             this.puppets[this.puppets.length - 1].actor.rgba = 0x010801D0
+
+            this.puppets[this.puppets.length - 1].actor.render_flags |= marina.render_flags & 0x160 | 0x800
+            this.puppets[this.puppets.length - 1].actor.mode |= marina.mode & 0x28 | 0x80800000
+            this.puppets[this.puppets.length - 1].actor.idle_time = 0xF
+            this.puppets[this.puppets.length - 1].actor.render_flags |= 0x10
+            this.ModLoader.emulator.rdramWrite32(i + 0x14C, free_slot)
+            this.puppets[this.puppets.length - 1].actor.status = 1
+            this.puppets[this.puppets.length - 1].actor.type = 0x60
 
             return free_slot
         }
